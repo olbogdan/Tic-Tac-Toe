76f275bdf58ad3a82dd2782e37be7d8a56a573b3
bogdanov <iceposhta@gmail.com>
2021-05-10 18:52:24 +0100

    record user moves to a collection

--- Tic-Tac-Toe/ContentView.swift
+++ Tic-Tac-Toe/ContentView.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isHumanTurn = true
+
     var body: some View {
         GeometryReader { geometry in
             VStack {
                 Spacer()
                 LazyVGrid(columns: columns) {
-                    ForEach(0 ..< 9) { _ in
+                    ForEach(0 ..< 9) { i in
                         ZStack {
                             Rectangle()
                                 .foregroundColor(.blue)
@@ -23,6 +30,9 @@ struct ContentView: View {
                             Image(systemName: "xmark")
                                 .resizable()
                                 .frame(width: geometry.size.width/3-50, height: geometry.size.width/3-50)
+                        }.onTapGesture {
+                            moves[i] = Move(player: isHumanTurn ? .human : .computer, boardIndex: i)
+                            isHumanTurn.toggle()
                         }
                     }
                 }

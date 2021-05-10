f0fd2af5cb13f351a9f8a5194f212cb5a36f6dc7
bogdanov <iceposhta@gmail.com>
2021-05-10 16:20:03 +0100

    Initial Commit

---
+++ Tic-Tac-Toe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tic-Tac-Toe
+//
+//  Created by bogdanov on 10.05.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
